dde3cd9f4d3ceff13bcadf35e54e5c529b7ad304
movahedi <daisy_m_18@yahoo.com>
2022-11-12 15:18:26 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>In the name of Allah.</h1>
+        <p>
+          This is only a test.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
