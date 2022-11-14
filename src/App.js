a3a5bacfaf772c67cb7c9e7e1d27fefa05a19bfe
daisym18 <daisy_m_18@yahoo.com>
2022-11-14 08:05:44 +0330

    some radio button have been added.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>In the name of Allah.</h1>
-        <label>Input Data</label>
-        <input type="text"/>
-        <input type="radio" value="would you like to click on this?"/> 
-        <input type="submit" value="Submit"/>
-          
+
+        <p>Please select your age:</p>
+        <input type="radio" id="age1" name="age" value="30" />
+        <label for="age1">0 - 30</label><br />
+        <input type="radio" id="age2" name="age" value="60" />
+        <label for="age2">31 - 60</label><br />
+        <input type="radio" id="age3" name="age" value="100" />
+        <label for="age3">61 - 100</label><br /><br />
+        <input type="submit" value="Submit" />
+
       </header>
     </div>
   );
 }
 
 export default App;
+
